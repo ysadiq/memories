a1a59e0ddc3f17ad5bc8689d24f286c50c9373ce
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-31 14:10:32 +0300

    using redux to dispatch getPosts action

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import Post from './Post/Post';
+import { useSelector } from 'react-redux';
 
+import Post from './Post/Post';
 import useStyles from './styles';
 
 const Posts = () => {
+    const posts = useSelector((state) => state.posts);
     const classes = useStyles();
     
     return (
---
+++ src/redux/actions/posts.js
@@ -0,0 +1,32 @@
+import * as api from '../../api';
+
+// Action Creators
+
+/* Action creators are simply functions that return an action, 
+and an action is just an object that has a type and payload.
+
+const getPosts = () => {
+    const action = { type: 'FETCH_ALL', payload: [] }
+    return action;
+}
+
+Since getPost is asynchronous logic we have to use 
+Redux Thunk as shown below:
+
+const getPosts = () => async (dispatch) => {    
+    const action = { type: 'FETCH_ALL', payload: [] }
+
+    dispatch(action);
+}
+*/
+
+export const getPosts = () => async (dispatch) => {   
+    try {
+        const { data } = await api.fetchPosts();
+        
+        dispatch({ type: 'FETCH_ALL', payload: data });
+    } catch (error) {
+        console.log(error.message);
+    } 
+
+}
