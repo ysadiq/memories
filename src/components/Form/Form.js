cc457f2113c6b4417d705d9f03f8621926afb842
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-31 15:32:44 +0300

    Use redux to dispatch createPost action

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
+import { useDispatch } from 'react-redux';
+
 import useStyles from './styles';
+import { createPost } from '../../redux/actions/posts';
 
 const Form = () => {
     const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: ''});
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
-        
+    const handleSubmit = (e) => {
+        // Prevent browser refresh
+        e.preventDefault();
+
+        dispatch(createPost(postData));
     }
 
     const clear = () => {
