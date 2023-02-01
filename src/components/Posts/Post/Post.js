480592b3eba073ba3a28e7f69dfe7a378b672df8
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-01 11:02:08 +0300

    Implement Like a Post action

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -4,11 +4,10 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
-
 import useStyles from './styles';
 import { useDispatch } from 'react-redux';
 
-import { deletePost } from '../../../redux/actions/posts';
+import { deletePost, likePost } from '../../../redux/actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -37,7 +36,7 @@ const Post = ({ post, setCurrentId }) => {
                 <Typography variant='h5' gutterBottom>{post.message}</Typography>
             </CardContent>
             <CardActions className={classes.cardActions}>
-                <Button size='small' color='primary' onClick={() => {}}>
+                <Button size='small' color='primary' onClick={() => dispatch(likePost(post._id))}>
                     <ThumbUpAltIcon fontSize='small' />
                     Like
                     {post.likeCount}
