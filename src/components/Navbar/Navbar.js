53093f1f3b6dd00ab61f52084ba46bdd328054e8
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-02 11:41:39 +0300

    Implement Logout

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,23 @@
 import React, { useState }from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
-
+import { useDispatch } from 'react-redux';
 import useStyles from './styles';
 import memories from '../../images/memories.png';
+import { LOGOUT } from '../../redux/constants/ActionTypes';
 
 const Navbar = () => {
     const classes = useStyles();
-
     const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    const logout = () => {
+        dispatch({ type: LOGOUT });
+
+        history.push('/');
+        setUser(null);
+    }
 
     // useEffect(() => {
     //     setUser(JSON.parse(localStorage.getItem('profile')));
@@ -30,7 +39,7 @@ const Navbar = () => {
                                 {user.result.name.charAt(0)}
                         </Avatar>
                         <Typography className={classes.username} variant='h6'>{user.result.name}</Typography>
-                        <Button variant='contained' className={classes.logout} color='secondary'>Logout</Button>
+                        <Button variant='contained' className={classes.logout} color='secondary' onClick={logout} >Logout</Button>
                     </div>
                 ) : (
                     <Button component={Link} to='/auth' variant='contained' color='primary'>Login</Button>
