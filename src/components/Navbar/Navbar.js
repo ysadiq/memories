d7bc352bf206201f748b072b840459e3da4b94f7
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-04 13:43:46 +0300

    enhance Navbar, background and Post UI

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,8 @@ import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
 import useStyles from './styles';
-import memories from '../../images/memories.png';
+import memoriesLogo from '../../images/memories-Logo.png';
+import memoriesText from '../../images/memories-Text.png';
 import { LOGOUT } from '../../redux/constants/ActionTypes';
 
 const Navbar = () => {
@@ -35,10 +36,10 @@ const Navbar = () => {
 
     return (
         <AppBar className={classes.appBar} position='static' color='inherit'>
-            <div className={classes.brandContainer}>
-                <Typography component={Link} to='/' className={ classes.heading } variant='h2' align='center'>Memories</Typography>
-                <img className={classes.image} src={memories} alt='memories' height='60' />
-            </div>
+            <Link to='/' className={classes.brandContainer}>
+                <img src={memoriesText} alt='icon' height='45px' />
+                <img className={classes.image} src={memoriesLogo} alt='icon' height='45px' />
+            </Link>
             <Toolbar className={classes.toolbar}>
                 {user ? (
                     <div className={classes.profile}>
