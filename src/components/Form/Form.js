db1fa6c78c39b716051ce67769a78c6e102db588
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-02-05 10:20:59 +0300

    enhance form UI

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -48,9 +48,9 @@ const Form = ({ currentId, setCurrentId }) => {
     }
 
     return (
-        <Paper className={classes.paper}>
+        <Paper className={classes.paper} elevation={6}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography variant='h6'>{ currentId ? 'Editing' : 'Creating' } a Memory</Typography>
+                <Typography variant='h6'>{ currentId ? `Editing ${post.title}` : 'Creating a Memory' } </Typography>
                 <TextField 
                     name='title' 
                     variant='outlined' 
